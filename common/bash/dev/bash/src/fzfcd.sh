28a52117fa1342ff9e437c318934ba0803862566
Itakashiraiya <79550547+itakashiraiya@users.noreply.github.com>
2025-01-26 20:07:24 +0000

    updated fzfcd to work inside .dirs

--- common/bash/dev/bash/src/fzfcd.sh
+++ common/bash/dev/bash/src/fzfcd.sh
@@ -1,4 +1,4 @@
 fzfcd() {
     local start_dir="${1:-$HOME/dev}"
-    cd "$(find "$start_dir" -type d -not -path "*/.*" | fzf)"
+    cd "$start_dir$(find "$start_dir" -type d -not -path "$start_dir*/.*" | sed "s|^$start_dir||" | fzf)"
 }
